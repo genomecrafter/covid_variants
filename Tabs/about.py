021689a2b161a982adaf69c9949a1c14043c547b
Nikita S Raj Kapini <nikitasraj16@gmail.com>
2025-03-10 18:15:35 +0530

    Update about.py

--- Tabs/about.py
+++ Tabs/about.py
@@ -13,7 +13,7 @@ def about_page():
         /* Header styling */
         h1, h2 {
             text-align: center;
-            color: #1F4E79;
+            color: black !important; /* Ensure text is black */
             font-weight: bold;
         }
 
@@ -27,7 +27,14 @@ def about_page():
             width: 70%;
             text-align: center;
             font-size: 18px;
-            color: black; /* Ensure text is visible */
+            color: black !important; /* Ensure text is visible */
+        }
+
+        /* Ensure captions are black */
+        .caption {
+            color: black !important;
+            text-align: center;
+            font-size: 16px;
         }
         </style>
         """,
@@ -35,21 +42,21 @@ def about_page():
     )
 
     # Page Title
-    st.markdown("<h1>About This Project</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='color: black;'>About This Project</h1>", unsafe_allow_html=True)
 
-    col_logo, col_logo1,col_logo2 = st.columns([1,1,5])
+    col_logo, col_logo1, col_logo2 = st.columns([1, 1, 5])
     with col_logo1:
-        st.image("images/logo-rvce.png", use_container_width=True, caption="RV College of Engineering")  # Update with actual image path
+        st.image("images/logo-rvce.png", use_container_width=True, caption="RV College of Engineering")  
     with col_logo2:
-        st.image("images/coe.png", width=600, caption="Computational Genomics Centre of Excellence")  # Update with actual image path
+        st.image("images/coe.png", width=600, caption="Computational Genomics Centre of Excellence")  
 
     # Side-by-side layout for sections
     col1, col2 = st.columns(2)
     with col1:
-        st.markdown("<h2>Data Collection</h2>", unsafe_allow_html=True)
+        st.markdown("<h2 style='color: black;'>Data Collection</h2>", unsafe_allow_html=True)
         st.markdown(
             '<div class="info-box">'
-            "<p>The dataset used in this project was collected by:<br>"
+            "<p style='color: black;'>The dataset used in this project was collected by:<br>"
             "<b>Computational Genomics Center of Excellence,</b><br>"
             "<b>Department of Biotechnology,</b><br>"
             "<b>RV College of Engineering.</b><br><br>"
@@ -59,10 +66,10 @@ def about_page():
         )
 
     with col2:
-        st.markdown("<h2>Webpage Development</h2>", unsafe_allow_html=True)
+        st.markdown("<h2 style='color: black;'>Webpage Development</h2>", unsafe_allow_html=True)
         st.markdown(
             '<div class="info-box">'
-            "<p>This webpage was designed and developed by:<br>"
+            "<p style='color: black;'>This webpage was designed and developed by:<br>"
             "<b>Nikita S Raj Kapini,</b><br>"
             "<b>4th Sem, Department of Computer Science,</b><br>"
             "<b>RV College of Engineering.</b><br><br>"
@@ -73,7 +80,7 @@ def about_page():
 
     # Footer
     st.markdown(
-        "<p style='text-align: center; font-size: 16px; color: #1F4E79;'>"
+        "<p style='text-align: center; font-size: 16px; color: black;'>"
         "For any queries, please contact: <b>nikitasrajk.cs23@rvce.edu.in</b></p>",
         unsafe_allow_html=True
     )
